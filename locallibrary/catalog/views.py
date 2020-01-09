8952c40418e6970144c770c5e6e42ba308a5a5d3
Justin Coulter <justincoulter@icloud.com>
2020-01-08 22:42:43 -0500

    dragged files in

---
+++ locallibrary/catalog/views.py
@@ -0,0 +1,76 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from catalog.models import Book, Author, BookInstance, Genre
+from django.contrib.auth.decorators import login_required
+
+# @login_required
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    
+    # The 'all()' is implied by default.    
+    num_authors = Author.objects.count()
+    
+    # generate counts of books with a particular word
+    num_books_word = Book.objects.filter(title__contains='Party').count()
+
+    num_genres_word = Genre.objects.filter(name__contains='Fiction').count()
+
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_books_word': num_books_word,
+        'num_genres_word': num_genres_word,
+        'num_visits': num_visits,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+class BookListView(LoginRequiredMixin,generic.ListView):
+    model = Book
+    paginate_by = 10
+
+
+
+class BookDetailView(LoginRequiredMixin,generic.DetailView):
+    model = Book
+
+# trying to figure out author details
+class AuthorListView(LoginRequiredMixin,generic.ListView):
+    model = Author
+
+    
+class AuthorDetailView(LoginRequiredMixin,generic.DetailView):
+    model = Author
+
+class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
+    """Generic class-based view listing books on loan to current user."""
+    model = BookInstance
+    template_name ='catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+    
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
+
+# class MyView(LoginRequiredMixin, View):
+#     login_url = '/login/'
+#     redirect_field_name = 'redirect_to'
